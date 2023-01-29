653a5a4e9d3e8243d7a2ac263bad31934475a2b6
Waqar Khan <sms.raees@gmail.com>
2023-01-30 03:24:12 +0500

    added postgres

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -91,8 +92,6 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
-
-from decouple import config
 
 DATABASES = {
     "default": {
